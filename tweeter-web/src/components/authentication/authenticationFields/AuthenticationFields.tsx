edbf0af090e6b2c4b3d9898fedd86da621aa1913
sam <samgerlach72@gmail.com>
2024-02-14 23:38:20 -0700

    Adding FollowingPresenter, FollowerPresenter, UserItemPresenter, UserItemService

---
+++ tweeter-web/src/components/authentication/authenticationFields/AuthenticationFields.tsx
@@ -0,0 +1,34 @@
+interface Props {
+    aliasfn: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    passwordfn: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const AuthenticationFields = (props: Props) => {
+    return(
+        <>
+            <div className="form-floating">
+                <input
+                type="text"
+                className="form-control"
+                size={50}
+                id="aliasInput"
+                placeholder="name@example.com"
+                onChange={(event) => props.aliasfn(event)}
+                />
+                <label htmlFor="aliasInput">Alias</label>
+            </div>
+            <div className="form-floating">
+                <input
+                type="password"
+                className="form-control"
+                id="passwordInput"
+                placeholder="Password"
+                onChange={(event) => props.passwordfn(event)}
+                />
+                <label htmlFor="passwordInput">Password</label>
+            </div>
+        </>
+    );
+}
+
+export default AuthenticationFields;
---
+++ tweeter-web/src/components/statusItem/StatusItem.tsx
@@ -0,0 +1,47 @@
+import { Status } from "tweeter-shared";
+import { Link } from "react-router-dom";
+import Post from "../statusItem/Post";
+import useUserNavigationHook from "../UserNavigationHook";
+
+interface Props {
+    item: Status;
+}
+
+const StatusItem = (props: Props) => {
+  const navigateToUser = useUserNavigationHook();
+  return(
+      <div className="col bg-light mx-0 px-0">
+      <div className="container px-0">
+        <div className="row mx-0 px-0">
+          <div className="col-auto p-3">
+            <img
+              src={props.item.user.imageUrl}
+              className="img-fluid"
+              width="80"
+              alt="Posting user"
+            />
+          </div>
+          <div className="col">
+            <h2>
+              <b>
+                {props.item.user.firstName} {props.item.user.lastName}
+              </b>{" "}
+              -{" "}
+              <Link
+                to={props.item.user.alias}
+                onClick={(event) => navigateToUser(event)}
+              >
+                {props.item.user.alias}
+              </Link>
+            </h2>
+            {props.item.formattedDate}
+            <br />
+            <Post status={props.item} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StatusItem;
